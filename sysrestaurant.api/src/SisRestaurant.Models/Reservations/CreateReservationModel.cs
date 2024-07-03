5c4e562b1776593d574d3e812a5c99f8e79720c2
Guilherme Kilian <guilherme.kilian@umbler.com>
2024-07-02 21:53:12 -0300

    build

--- sysrestaurant.api/src/SisRestaurant.Models/Reservations/CreateReservationModel.cs
+++ sysrestaurant.api/src/SisRestaurant.Models/Reservations/CreateReservationModel.cs
@@ -8,6 +8,8 @@ namespace SisRestaurant.Models.Reservations
 {
     public class CreateReservationModel
     {
+        public int RestaurantId { get; set; }
+
         public DateTime Date { get; set; }
 
         public int Count { get; set; }
